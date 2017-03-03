fc245a3d29c72ec5821ff20a0e9a7615455e2398
Wade Wilson <wadewilson@Wades-MacBook-Pro.local>
2017-03-03 10:20:42 -0800

    Initial Commit

---
+++ Project Beta/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Project Beta
+//
+//  Created by Wade Wilson on 3/3/17.
+//  Copyright © 2017 Wade Wilson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
