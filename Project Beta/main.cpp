4ae7e3274f4e9d3a800283eda85ac0a92843856c
Wade Wilson <wadewilson@216.200.96.25.ipyx-116884-zyo.zip.zayo.com>
2017-03-13 22:23:57 -0700

    updated for HW3

--- Project Beta/main.cpp
+++ Project Beta/main.cpp
@@ -36,8 +36,10 @@ class agent{
 public:
     int xpos ;
     int ypos ;
-    int ix ;
-    int iy ;
+    int lastx;
+    int lasty;
+    int ix =2;
+    int iy=2 ;
     int dx;
     int dy;
     int pstate;
@@ -58,6 +60,9 @@ public:
     void update(qlearner *pqt, grid);
     void reset();
     int choice2();
+    bool wall();
+    void wallcheck(grid);
+    int choice3(grid);
 };
 
 class body{
@@ -67,6 +72,11 @@ public:
     
 };
 
+void agent::reset(){
+    xpos = ix;
+    ypos = iy;
+    win= false;
+}
 
 void Qsetup(qlearner* pqt){
     int num_state=100;
@@ -85,8 +95,6 @@ void Qsetup(qlearner* pqt){
             pqt->Q.at(i).at(j)=.001*LYRAND;
         }
     }
-
-    
 }
 
 void Qsetup2(qlearner* pqt){
@@ -134,6 +142,8 @@ int agent::choice2(){//0 up 1 down 2 left 3 right
 
 void agent::move(grid world){ // will accept movement commands
     pinput=input;
+    lastx=xpos;
+    lasty=ypos;
     if(input == 0){
         if(ypos >= world.ymax){
             ypos = world.ymax;
@@ -188,6 +198,33 @@ void agent::check(grid world){
     }
 }
 
+void agent::wallcheck(grid world){
+    bool stop = false;
+    stop = wall();
+    if(stop){
+        //cout<< " Cannot move into that square, there is a wall there" << endl;
+        if((pinput == 2) || ( pinput == 3)){
+            xpos=lastx;
+        }
+        else if((pinput == 0) || (pinput == 1)){
+            ypos=lasty;
+        }
+    }
+    /*
+    if(xpos >= world.xmax){
+        xpos = world.xmax;
+    }
+    if(xpos <= world.xmin){
+        xpos = world.xmin;
+    }
+    if(ypos <= world.ymin){
+        ypos = world.ymin;
+    }
+    if(ypos >= world.ymax){
+        ypos = world.ymax;
+    }
+     */
+}
 void agent::eval(grid world){
     bool xwin =false;
     bool ywin = false;
@@ -212,16 +249,16 @@ void agent::sense2(grid world){
     pstate=state;
     dx=xpos-world.goalx;
     dy=ypos-world.goaly;
-    if(dx>0 & dy > 0){
+    if(dx>0 && dy > 0){
         state=dy*world.xmax+dx;
     }
-    if(dx>0 & dy<0){
+    if(dx>0 && dy<0){
         state=100+(-1*dy*world.xmax+dx);
     }
-    if(dx<0 & dy >0){
+    if(dx<0 && dy >0){
         state=200+(dy*world.xmax+dx*-1);
     }
-    if(dx<0 & dy <0){
+    if(dx<0 && dy <0){
         state=300+(-1*dy*world.xmax+dx*-1);
     }
     //state = ypos*world.xmax+xpos;
@@ -246,6 +283,52 @@ int agent::choice(qlearner* pqt){  // Greedy choice with small exploration
     return sto;
 }
 
+int agent::choice3(grid world){
+    int input=0;
+    if(world.goalx > xpos){ // robot is left of goal
+        input = 3;
+    }
+    if(world.goalx < xpos){ // robot is right of goal
+        input = 2;
+    }
+    if(world.goaly > ypos){ // robot is below goal
+        input = 0;
+    }
+    if(world.goaly < ypos){ // robot is above goal
+        input = 1;
+    }
+    return input;
+}
+
+
+bool agent::wall(){
+    bool result = false;
+    if(xpos == 1 && ypos == 3){
+        result = true;
+    }
+    else if(xpos == 2 && ypos == 3){
+        result = true;
+    }
+    else if(xpos == 3 && ypos == 3){
+        result = true;
+    }
+    else if(xpos == 3 && ypos == 1){
+        result = true;
+    }
+    else if(xpos == 3 && ypos == 2){
+        result = true;
+    }
+    /*else if(xpos == 0 && ypos == 3){
+        result = true;
+    }
+    else if(xpos == 3 && ypos == 0){
+        result = true;
+    }
+    */
+    
+    return result;
+}
+
 void agent::update(qlearner* pqt, grid world){ //updates action values
     double alpha =.1;
     double gamma = .9;
@@ -262,7 +345,54 @@ void agent::update(qlearner* pqt, grid world){ //updates action values
     pqt->Q.at(pstate).at(pinput)=pqt->Q.at(pstate).at(pinput)+alpha*(reward+gamma*qmax - pqt->Q.at(pstate).at(pinput));
    // cout << reward << endl;
 }
-void learningCurve(int x, int y, int episodes){
+void testA(int x, int y, int episodes){
+    cout << "Test A Start" << endl;
+    agent robot;
+    grid world;
+    world.xmax=x-1;
+    world.ymax=y-1;
+    world.goalx=8;
+    world.goaly=8;
+    int hint = 10000;
+    bool lose = false;
+    
+    int stat_runs=30;
+    int counting;
+    bool no = false;
+    for(int s=0; s<stat_runs;s++){
+            robot.reset();
+            int counting = 0;
+            bool notwinner=true;
+            for(int p=0; p<hint; p++){
+                //cout << robot.xpos <<"\t"<<robot.ypos<<endl;
+                robot.wall();
+                robot.check(world);
+                robot.input = robot.choice3(world);
+                robot.move(world);
+                //cout<<robot.xpos<<"\t"<<robot.ypos<<endl;
+                no = robot.wall();
+                if(no){
+                    robot.wallcheck(world);
+                    //assert(robot.xpos < 4);
+                }
+                else{
+                    robot.eval(world);
+                        counting++;
+                if( (robot.xpos == world.goalx) && (robot.ypos == world.goaly) ){
+                    bool lose = true;
+                    //cout << "heuristic found goal, test A failed" << endl;
+                    break;
+                }
+                }
+            }
+        }
+    
+    assert(lose == false);
+    cout << "Test A is successful if this is outputted" << endl;
+}
+
+void TestB(int x, int y, int episodes){
+    cout << "Test B start" << endl;
     agent robot;
     grid world;
     qlearner qt;
@@ -271,7 +401,7 @@ void learningCurve(int x, int y, int episodes){
     Qsetup(pqt);
     world.xmax=x-1;
     world.ymax=y-1;
-    world.goalx=4;
+    world.goalx=8;
     world.goaly=8;
     
     int stat_runs=30;
@@ -279,25 +409,31 @@ void learningCurve(int x, int y, int episodes){
     ofstream outputFile;
     outputFile.open("LCout.txt");
     int counting;
+    bool no = false;
     for(int s=0; s<stat_runs;s++){
         Qsetup(pqt);  //resets Q table for each stat run to generate a learning curve over the # of episodes
         for(int c=0; c<episodes; c++){
-            robot.xpos=1;
-            robot.ypos=1;
-            robot.win=false;
+            robot.reset();
             int counting = 0;
             bool notwinner=true;
             while(notwinner != robot.win){
+                //cout << robot.xpos <<"\t"<<robot.ypos<<endl;
+                robot.wall();
                 robot.check(world);
-                robot.eval(world);
                 robot.sense(world);
                 robot.input = robot.choice(pqt);
                 robot.move(world);
+                //cout<<robot.xpos<<"\t"<<robot.ypos<<endl;
+                no = robot.wall();
+                if(no){
+                    robot.wallcheck(world);
+                    //assert(robot.xpos < 4);
+                }
+                else{
                 robot.eval(world);
                 robot.sense(world);
                 robot.update(pqt, world);
-                robot.check(world);
-                robot.eval(world);
+                }
                 counting++;
             }
             
@@ -312,7 +448,8 @@ void learningCurve(int x, int y, int episodes){
     
     
     outputFile.close();
-    cout << "Learning Curve output to text file" << endl;
+    //cout << "Learning Curve output to text file" << endl;
+    cout << "Learning Agent completed the grid with walls " << stat_runs << " times" << endl;
 }
 
 void testD(int x, int y, int episodes){
@@ -332,9 +469,7 @@ void testD(int x, int y, int episodes){
     for(int s=0; s<stat_runs;s++){
         Qsetup(pqt);
         for(int c=0; c<episodes; c++){
-            robot.xpos=1;
-            robot.ypos=1;
-            robot.win=false;
+            robot.reset();
             int counting = 0;
             bool notwinner=true;
             while(notwinner != robot.win){
@@ -361,11 +496,6 @@ void testD(int x, int y, int episodes){
     cout<<"Test D passed"<<endl;
 }
 
-void agent::reset(){
-    xpos = ix;
-    ypos = iy;
-    win= false;
-}
 
 void testE(int x, int y, int episodes){
     agent robot;
@@ -378,8 +508,8 @@ void testE(int x, int y, int episodes){
     world.ymax=y-1;
     world.goalx=4;
     world.goaly=8;
-    robot.ix=2;
-    robot.iy=1;
+    robot.ix=3;
+    robot.iy=3;
     int stat_runs=30;
     
     for(int s=0; s<stat_runs;s++){
@@ -389,7 +519,7 @@ void testE(int x, int y, int episodes){
             //robot.ypos=robot.iy;
             //robot.win=false;
             robot.reset();
-            assert(robot.xpos == robot.ix & robot.ypos == robot.iy & not(robot.win));
+            assert(robot.xpos == robot.ix && robot.ypos == robot.iy && not(robot.win));
             int counting = 0;
             bool notwinner=true;
             while(notwinner != robot.win){
@@ -447,10 +577,10 @@ void testF(int x, int y, int episodes){
                 robot.check(world);
                 robot.eval(world);
                 counting++;
-                if(s==0 & c==0){
+                if(s==0 && c==0){
                     icount=counting;
                 }
-                if(s==(stat_runs-1) & c==(episodes-1)){
+                if(s==(stat_runs-1) && c==(episodes-1)){
                     fcount=counting;
                 }
             }
@@ -501,10 +631,10 @@ void testG(int x, int y, int episodes){
                 robot.check(world);
                 robot.eval(world);
                 counting++;
-                if(s==0 & c==0){
+                if(s==0 && c==0){
                     icount=counting;
                 }
-                if(s==(stat_runs-1) & c==(episodes-1)){
+                if(s==(stat_runs-1) && c==(episodes-1)){
                     fcount=counting;
                 }
             }
@@ -522,13 +652,14 @@ void testG(int x, int y, int episodes){
 
 int main(){
     cout << "main start" << endl;
-    srand(time(NULL));
-    int episodes=500;
+    srand((unsigned)time(NULL));
+    int episodes=5000;
     int x=10;
     int y=10;
-    learningCurve(x, y, episodes);
-    testD(x, y, episodes);
-    testE(x,y,episodes);
-    testF(x,y,episodes);
-    testG(x,y,episodes);
+    testA(x,y,episodes);
+    TestB(x, y, episodes);
+    //testD(x, y, episodes);
+    //testE(x,y,episodes);
+    //testF(x,y,episodes);
+    //testG(x,y,episodes);
 }
